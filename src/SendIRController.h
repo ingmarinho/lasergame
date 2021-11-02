1d8cac01ca7853c9b95f95660b201b625e61ea08
MoHunterX <64601947+MoHunterX@users.noreply.github.com>
2021-11-02 14:05:54 +0100

    IRSendController

--- src/SendIRController.h
+++ src/SendIRController.h
@@ -1,52 +1,50 @@
 #pragma once
 
-class Led
+class led
 {
 private:
-	hwlib::target::pin_out &led;
-
+	hwlib::target::pin_out &led
 public:
-	Led(hwlib::target::pin_out &led)
-		: led(led)
+	led( hwlib::target::pin_out &led)
+		: led(red)
 	{
 	}
-
-	void ZetAan()
-	{
+	
+	void ZetAan(){
 		led.write(1);
 		led.flush();
 	}
-
-	void ZetUit()
-	{
+	
+	void ZetUit(){
 		led.write(0);
 		led.flush();
 	}
 };
 
+
+
 class IRTransmitter
 {
 private:
-	hwlib::target::d2_36kHz &IR;
+	hwlib::target::d2_36kHz &ir;
 
 public:
-	IRTransmitter(hwlib::target::d2_36kHz &IR)
-		: IR(IR)
+	IRTransmitter(hwlib::target::d2_36kHz &ir)
+		: ir(ir)
 	{
 	}
-
-	void ZetAan()
-	{
-		IR.write(1);
-		IR.flush();
+	
+	void ZetAan(){
+		ir.write(1);
+		ir.flush();
 	}
-
-	void ZetUit()
-	{
-		IR.write(0);
-		IR.flush();
+	
+	void ZetUit(){
+		ir.write(0);
+		ir.flush();
 	}
 };
+
 
 class SendIRController : public rtos::task<>
 {
@@ -57,61 +55,99 @@ class SendIRController : public rtos::task<>
 	};
 	enum state_s
 	{
-		GETBIT,
-		SEND1_SIGNAL,
-		SEND1_PAUSE,
-		SEND0_SIGNAL,
-		SEND0_PAUSE
+		getbit ,
+		send1_signal,
+		send1_pause,
+		send0_signal,
+		send0_pause
 	};
 
 private:
 	state_t state = IDLE;
-	state_s stateS = GETBIT;
+	state_s stateS = getbit
 	IRTransmitter IR;
-	Led red;
+	led Red;
+	rtos::channel<uint16_t, 1024> sendChannel;
+	int sendcounter = 0;
+	int huidig = 0;
+	uint16_t msg = 0x0;
+	rtos::timer delay;
 
 public:
-	SendIRController(hwlib::target::d2_36kHz &IR, hwlib::target::pin_out &red)
-		: IR(IR), red(red)
+	SendIRController(hwlib::target::d2_36kHz &ir, hwlib::target::pin_out &red)
+		: IR(ir), Red(red)
 	{
 	}
-
+	
+	void SendMessage(uint16_t msg){
+		sendChannel.write(msg);
+	}
+	
 private:
 	void main()
 	{
-
 		for (;;)
 		{
 			switch (state)
 			{
-			case IDLE:
-
-				break;
-
-			case SENDBITS:
-				switch (stateS)
-				{
-				case GETBIT:
-
+				case IDLE:
+					wait(sendChannel);
+					msg = sendChannel.read();
+					state = SENDBITS;
 					break;
-
-				case SEND0_PAUSE:
-
+				
+				case SENDBITS:
+					switch (stateS)
+					{
+						case getbit:
+							if(huidig >= 15){
+								huidig = 0;
+								sendcounter++;
+								if(sendcounter == 2) {
+									sendcounter = 0;
+									state = IDLE;
+								}
+							}
+							uint16_t masker = 0x1;
+							masker = masker << huidig;
+							uint16-t res = masker & msg;
+							huidig++;
+							if(res > 0){
+								stateS = send1_signal;
+							}else{
+								stateS = send0_signal;
+							}
+							break;
+						
+						case send0_pause:
+							IR.ZetUit();
+							delay.set(1600);
+							wait(delay);
+							stateS = getbit;
+							break;
+						
+						case send0_signal:
+							IR.ZetAan();
+							delay.set(800);
+							wait(delay);
+							stateS = send0_pause;
+							break;
+						
+						case send1_pause:
+							IR.ZetUit();
+							delay.set(800);
+							wait(delay);
+							stateS = getbit;
+							break;
+						
+						case send1_signal:
+							IR.ZetAan();
+							delay.set(1600);
+							wait(delay);
+							stateS = send1_pause
+							break;
+					}
 					break;
-
-				case SEND0_SIGNAL:
-
-					break;
-
-				case SEND1_PAUSE:
-
-					break;
-
-				case SEND1_SIGNAL:
-
-					break;
-				}
-				break;
 			}
 		}
 	}
