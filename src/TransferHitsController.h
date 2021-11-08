f7c105d8502dba2b766121bf4dd1bc984d9352c4
TheWezzz <wesley.vanvliet@student.hu.nl>
2021-11-08 14:03:01 +0100

    TransferHitController

--- src/TransferHitsController.h
+++ src/TransferHitsController.h
@@ -11,12 +11,39 @@ class TransferHitsController : public rtos::task<>
 private:
     state_t state = IDLE;
 
+    rtos::flag(TransferHitsFlag);
+
+    Hitlog hitlog;
+    auto Data[];
+    TransferHitsController hits;
+
 public:
+    TransferHitsController(unsigned int priority)
+    : rtos::task<>(priority, "TRANSFERHITSCONTROLLER_TASK"), hits(hits)
+    {
+    }
+
 private:
     void main()
     {
         for (;;)
         {
+            switch (state)
+            {
+                case IDLE:
+                    wait(TransferHitsFlag)
+
+                    if(TransferHitsFlag)
+                    {
+                        state = TRANSFERHITS;
+                    }
+                    break;
+
+                case TRANSFERHITS:
+                    Data = hitlog.getData;
+                    break;
+
+            }
         }
     }
 };
