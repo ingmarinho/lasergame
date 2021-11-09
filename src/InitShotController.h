9b312b7eb0a612cdb4858a8c91feeb4722f36120
IanRietveld <ian.rietveld@student.hu.nl>
2021-11-09 15:22:01 +0100

    ccc

--- src/InitShotController.h
+++ src/InitShotController.h
@@ -3,106 +3,97 @@
 class ButtonListener
 {
 public:
-	virtual void ButtonPressed(int buttonid);
+    virtual void ButtonPressed(int buttonid);
 };
 
-template< unsigned int maxNumberOfListeners>
-class button : public rtos::task<> 
+template <unsigned int maxNumberOfListeners>
+class button : public rtos::task<>
 {
 
 private:
-	std::array<ButtonListener*, maxNumberOfListeners> ButtonListeners;
+    std::array<ButtonListener *, maxNumberOfListeners> ButtonListeners;
     hwlib::target::pin_in Trigger;
-	unsigned int currentNumberOfListeners = 0;
+    unsigned int currentNumberOfListeners = 0;
 
 public:
-	
-    button(hwlib::target::pin_in &button, unsigned int priority) : 
-    rtos::task<>(priority, "BUTTON_TAAK"), Trigger(button)
+    button(hwlib::target::pin_in &button, unsigned int priority) : rtos::task<>(priority, "BUTTON_TAAK"), Trigger(button)
     {
-	    for(unsigned int i = 0 ; i < maxNumberOfListeners ; i++)
-	    {
-	    	ButtonListeners[i] = nullptr;
-	    }
+        for (unsigned int i = 0; i < maxNumberOfListeners; i++)
+        {
+            ButtonListeners[i] = nullptr;
+        }
     }
 
     void addListener(ButtonListener *listener)
-	{
-		if (currentNumberOfListeners < maxNumberOfListeners)
-		{
-			ButtonListeners[currentNumberOfListeners] = listener;
-			currentNumberOfListeners++;
-		}
-	}
-	
-	void main()
-	{
-		for(;;)
-		{
-			bool t = false;
-			
-			while(!t)
-			{
-				t = Trigger.read();
-			}
-			
-			for(unsigned int i = 0 ; i < maxNumberOfListeners ; i++)
-			{
-				auto listener = ButtonListeners[i];
-				if(listener!=nullptr)
-				{
-					listener->ButtonPressed(0);
-				}
-			}
-		}
-	}
-};
+    {
+        if (currentNumberOfListeners < maxNumberOfListeners)
+        {
+            ButtonListeners[currentNumberOfListeners] = listener;
+            currentNumberOfListeners++;
+        }
+    }
 
+    void main()
+    {
+        for (;;)
+        {
+            bool t = false;
+
+            while (!t)
+            {
+                t = Trigger.read();
+            }
+
+            for (unsigned int i = 0; i < maxNumberOfListeners; i++)
+            {
+                auto listener = ButtonListeners[i];
+                if (listener != nullptr)
+                {
+                    listener->ButtonPressed(0);
+                }
+            }
+        }
+    }
+};
 
 class InitShotController : public rtos::task<>
 {
 
 private:
-	
-	enum state_t
-	{
-		IDLE,
-		STARTGAME,
-		SENDIR,
-		ZOMBIE,
-		GAMEOVER
-		
-	};
-	
-	state_t state = STARTGAME;
-	rtos::channel<char, 1024> ButtonChannel;
-	rtos::timer Timer;
-	rtos::flag ZombieFlag;
-	rtos::flag StartGame;
-	rtos::flag GameOver;
-	uint16_t Commando;
-	int Delay;
-	button Trigger;
+    enum state_t
+    {
+        IDLE,
+        STARTGAME,
+        SENDIR,
+        ZOMBIE,
+        GAMEOVER
+
+    };
+
+    state_t state = STARTGAME;
+    rtos::channel<char, 1024> ButtonChannel;
+    rtos::timer Timer;
+    rtos::flag ZombieFlag;
+    rtos::flag StartGame;
+    rtos::flag GameOver;
+    uint16_t Commando;
+    int Delay;
+    button<1> Trigger;
 
 public:
-	
-	InitShotController(hwlib::target::pin_in buttontrigger, unsigned int priority) :
-			rtos::task<>(1, "TRIGGER_TAAK"), Trigger(buttontrigger)
-	{
-		unsigned int i=0;
-		for(i=0;i<maxNumberOfListeners;i++)
-		{
-			ButtonListeners[i]=nullptr;
-		}
-	}
-	
-	
-    InitGameController(Button &Button, unsigned int priority) :
-     rtos::task<>(priority, "BUTTON_TAAK"), ButtonChannel(this, "BUTTON_CHANNEL"), button(button),  
+    InitShotController(hwlib::target::pin_in buttontrigger, unsigned int priority) : rtos::task<>(1, "TRIGGER_TAAK"), Trigger(buttontrigger)
+    {
+        unsigned int i = 0;
+        for (i = 0; i < maxNumberOfListeners; i++)
+        {
+            ButtonListeners[i] = nullptr;
+        }
+    }
+
+    InitGameController(Button &Button, unsigned int priority) : rtos::task<>(priority, "BUTTON_TAAK"), ButtonChannel(this, "BUTTON_CHANNEL"), button(button),
     {
         button.addListener(this);
     }
-
 
     void ButtonPressed(int ButtonID)
     {
@@ -110,25 +101,23 @@ public:
     }
 
     void StartGame(uint16_t Command, int Delay)
-    {   
-        Command = 
-        Delay = Delay;
+    {
+        Command =
+            Delay = Delay;
 
-        
         StartGame.set();
     }
 
     void GameOver()
     {
         GameOver.set();
-    }   
+    }
 
     void ZombieFlag()
     {
-       ZombieFlag.set() 
+        ZombieFlag.set()
     }
 
-    
 private:
     void main()
     {
@@ -137,17 +126,17 @@ private:
             switch (state)
             {
             case STARTGAME:
-            wait(StartGame);
-            state = IDLE;
+                wait(StartGame);
+                state = IDLE;
                 break;
 
             case IDLE:
-            auto event = wait(GameOver + ZombieFlag + ButtonChannel)
+                auto event = wait(GameOver + ZombieFlag + ButtonChannel)
 
-            if(event = ZombieFlag)
-            {
-                state = ZOMBIE;
-            }
+                    if (event = ZombieFlag)
+                {
+                    state = ZOMBIE;
+                }
 
                 break;
 
@@ -156,14 +145,14 @@ private:
             {
                 hwlib::wait_ms(5000);
             }
-                
-                break;
+
+            break;
 
             case SENDIR:
-            
-            sendIRcontroller.sendMessage()
-                
-                break;
+
+                sendIRcontroller.sendMessage()
+
+                    break;
 
             case GAMEOVER:
             }
