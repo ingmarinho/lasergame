8242b3de36256da79874977e3a78d348925eea3b
MoHunterX <64601947+MoHunterX@users.noreply.github.com>
2021-11-09 17:03:32 +0100

    rungame

--- src/RunGameController.h
+++ src/RunGameController.h
@@ -3,15 +3,77 @@
 #include <array>
 
 template <unsigned int maxNumberOfWeapons>
-class damagelist {
+
+class DamageList {
 private:
-	std::array<int, maxNumberOfWeapons> firepowers;
+	
+	struct info{
+		string WeaponName;
+		int damage;
+		int delay;
+		info(string WeaponName, int damage, int delay) : WeaponName(WeaponName), damage(damage), delay(delay){}
+	};
+	
+	std::array<info, maxNumberOfWeapons> Weapons;
+	
 public:
-	damagelist(){
-		int damage = 10;
-		for(unsigned int i = 0 ; i < maxNumberOfWeapons ; i++){
+	DamageList(){
 		
+		info Weapon0("Knife", 10, 1'500'000);
+		Weapons[0] = Weapon0;
+		info Weapon1("Eagle", 15, 2'000'000);
+		Weapons[1] = Weapon1;
+		info Weapon2("Machine Gun", 5, 700'000);
+		Weapons[2] = Weapon2;
+		info Weapon3("AUG", 5, 500'000);
+		Weapons[3] = Weapon3;
+		info Weapon4("AK-47", 25, 3'500'000);
+		Weapons[4] = Weapon4;
+		info Weapon5("M4A4", 20, 3'000'000);
+		Weapons[5] = Weapon5;
+		info Weapon6("AWP", 40, 5'500'000);
+		Weapons[6] = Weapon6;
+		info Weapon7("M249", 3, 500'000);
+		Weapons[7] = Weapon7;
+		info Weapon8("Shotgun", 30, 4'000'000);
+		Weapons[8] = Weapon8;
+		info Weapon9("rbg", 50, 6'500'000);
+		Weapons[9] = Weapon9;
+	}
+	
+	const char * GetName(int i){
+		if(i < maxNumberOfWeapons){
+			return Weapons[i].WeaponName;
 		}
+	}
+	
+	int GetDamage(int i){
+		if(i < maxNumberOfWeapons){
+			return Weapons[i].damage;
+		}
+	}
+	
+	int GetDelay(int i){
+		if(i < maxNumberOfWeapons){
+			return Weapons[i].delay;
+		}
+	}
+};
+
+class Speeltijd
+{
+private:
+	int speeltijd = 0;
+public:
+	
+	SetGameTime(int time)
+	{
+		speeltijd = time;
+	}
+	
+	int GetGameTime()
+	{
+		return speeltijd;
 	}
 };
 
@@ -32,6 +94,11 @@ class RunGameController : public rtos::task<>
 private:
     state_t state = IDLE;
     ParametersController &parameterscontroller
+    Speeltijd speeltijd;
+    DamageList<10> damagelist;
+	InitShotController &initshotcontroller;
+	Led green;
+	ReceiveIRController &receiveircontroller;
 
 
 public:
