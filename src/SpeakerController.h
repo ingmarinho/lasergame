e83466016956f1cff03bb79aa450a989d3f833e2
TheWezzz <wesley.vanvliet@student.hu.nl>
2021-11-10 17:12:59 +0100

    Speakerrrr

--- src/SpeakerController.h
+++ src/SpeakerController.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "note_player_gpio.hpp"
+#include "rtos.hpp"
+#include "hwlib.hpp"
 
 
 enum Sounds
@@ -12,33 +13,35 @@ enum Sounds
 
 };
 
-//class Note
-//{
-//public:
-//    static const int A4 = 440;
-//    static const int A4s = 466;
-//    static const int B4 = 494;
-//    static const int C5 = 523;
-//    static const int C5s = 554;
-//    static const int D5 = 587;
-//    static const int D5s = 622;
-//    static const int E5 = 659;
-//    static const int F5 = 698;
-//    static const int F5s = 740;
-//    static const int G5 = 784;
-//    static const int G5s = 830;
-//    static const int A5 = 880;
-//    static const int A5s = 932;
-//    static const int B5 = 987;
-//
-//    const int frequency;
-//
-//    static const int dF = 1'000'000; // full measure
-//    static const int dH = dF / 2;    // half measure
-//    static const int dQ = dF / 4;    // quarter measure
-//
-//    const int duration;
-//};
+class note {
+public:
+
+    // from https://www.seventhstring.com/resources/notefrequencies.html
+    static const int A4  = 440;
+    static const int A4s = 466;
+    static const int B4  = 494;
+    static const int C5  = 523;
+    static const int C5s = 554;
+    static const int D5  = 587;
+    static const int D5s = 622;
+    static const int E5  = 659;
+    static const int F5  = 698;
+    static const int F5s = 740;
+    static const int G5  = 784;
+    static const int G5s = 830;
+    static const int A5  = 880;
+    static const int A5s = 932;
+    static const int B5  = 987;
+
+    const int frequency;
+
+    static const int dF = 1'000'000;  // full measure
+    static const int dH = dF / 2;     // half measure
+    static const int dQ = dF / 4;     // quarter measure
+
+    const int duration;
+};
+
 
 class Speaker
 {
@@ -47,7 +50,7 @@ private:
 
 public:
     Speaker(hwlib::target::pin_out &speaker)
-        : speaker(speaker)
+            : speaker(speaker)
     {
     }
 
@@ -62,6 +65,32 @@ public:
         speaker.flush();
     }
 };
+
+
+class note_player_gpio{
+private:
+    Speaker &lsp;
+public:
+    note_player_gpio(Speaker &lsp) : lsp( lsp )
+    {}
+
+    void play( const note & n ){
+        if( n.frequency == 0 ){
+            hwlib::wait_us( n.duration );
+
+        } else {
+            auto half_period = 1'000'000 / ( 2 * n.frequency );
+            auto end = hwlib::now_us() + n.duration;
+            do {
+                lsp.turnOn();
+                hwlib::wait_us( half_period );
+                lsp.turnOff();
+                hwlib::wait_us( half_period );
+            } while ( end > hwlib::now_us() );
+        }
+    }
+};
+
 
 class SpeakerController : public rtos::task<>
 {
@@ -79,12 +108,12 @@ private:
     rtos::channel<Sounds, 1024> soundIDChannel;
 
     Speaker speaker;
-	note_player_gpio player;
+    note_player_gpio player;
     // Note note;
 
 public:
     SpeakerController(hwlib::target::pin_out &speaker1, unsigned int priority)
-        : rtos::task<>(priority, "SPEAKERCONTROLLER_TASK"), soundIDChannel(this, "SOUNDS_CHANNEL"), speaker(speaker1), player(speaker)
+            : rtos::task<>(priority, "SPEAKERCONTROLLER_TASK"), soundIDChannel(this, "SOUNDS_CHANNEL"), speaker(speaker1), player(speaker)
     {
     }
 
@@ -94,25 +123,25 @@ public:
     }
 
 private:
-    void playNote(const Note &n)
-    {
-        if (n.frequency == 0)
-        {
-            hwlib::wait_us(n.duration);
-        }
-        else
-        {
-            auto half_period = 1'000'000 / (2 * n.frequency);
-            auto end = hwlib::now_us() + n.duration;
-            do
-            {
-                speaker.turnOn();
-                hwlib::wait_us(half_period);
-                speaker.turnOff();
-                hwlib::wait_us(half_period);
-            } while (end > hwlib::now_us());
-        }
-    }
+//    void playNote(const note &n)
+//    {
+//        if (n.frequency == 0)
+//        {
+//            hwlib::wait_us(n.duration);
+//        }
+//        else
+//        {
+//            auto half_period = 1'000'000 / (2 * n.frequency);
+//            auto end = hwlib::now_us() + n.duration;
+//            do
+//            {
+//                speaker.turnOn();
+//                hwlib::wait_us(half_period);
+//                speaker.turnOff();
+//                hwlib::wait_us(half_period);
+//            } while (end > hwlib::now_us());
+//        }
+//    }
 
     void hitSound()
     {
@@ -120,90 +149,87 @@ private:
 //        hwlib::wait_ms(500);
 //        speaker.turnOff();
 //        hwlib::wait_ms(500);
-        // playNote(note{note::E5, note::dQ});
-        // playNote(note{note::D5s, note::dQ});
-        // playNote(note{note::E5, note::dQ});
-        // playNote(note{note::D5s, note::dQ});
-        // playNote(note{note::E5, note::dQ});
-        // playNote(note{note::B4, note::dQ});
-        // playNote(note{note::D5, note::dQ});
-        // playNote(note{note::C5, note::dQ});
-        // playNote(note{note::A4, note::dH});
-	
-	
-	    player.play( note{ 523, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 698, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 880, 125000 } );
-	    player.play( note{ 0, 125000 } );
-	    player.play( note{ 0, 62500 } );
-	    player.play( note{ 880, 250000 } );
-	    player.play( note{ 0, 125000 } );
-	    player.play( note{ 784, 500000 } );
-	    player.play( note{ 523, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 698, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 784, 125000 } );
-	    player.play( note{ 0, 125000 } );
-	    player.play( note{ 0, 62500 } );
-	    player.play( note{ 784, 250000 } );
-	    player.play( note{ 0, 125000 } );
-	    player.play( note{ 698, 250000 } );
-	    player.play( note{ 659, 125000 } );
-	    player.play( note{ 587, 250000 } );
-	    player.play( note{ 523, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 698, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 698, 500000 } );
-	    player.play( note{ 784, 250000 } );
-	    player.play( note{ 659, 250000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 523, 250000 } );
-	    player.play( note{ 261, 250000 } );
-	    player.play( note{ 523, 250000 } );
-	    player.play( note{ 784, 250000 } );
-	    player.play( note{ 0, 250000 } );
-	    player.play( note{ 698, 1000000 } );
-	    player.play( note{ 523, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 698, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 880, 125000 } );
-	    player.play( note{ 0, 125000 } );
-	    player.play( note{ 0, 62500 } );
-	    player.play( note{ 880, 250000 } );
-	    player.play( note{ 0, 125000 } );
-	    player.play( note{ 784, 500000 } );
-	    player.play( note{ 523, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 698, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 1046, 500000 } );
-	    player.play( note{ 659, 250000 } );
-	    player.play( note{ 698, 250000 } );
-	    player.play( note{ 659, 125000 } );
-	    player.play( note{ 587, 250000 } );
-	    player.play( note{ 523, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 698, 125000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 698, 500000 } );
-	    player.play( note{ 784, 250000 } );
-	    player.play( note{ 659, 250000 } );
-	    player.play( note{ 587, 125000 } );
-	    player.play( note{ 523, 250000 } );
-	    player.play( note{ 261, 250000 } );
-	    player.play( note{ 523, 250000 } );
-	    player.play( note{ 784, 250000 } );
-	    player.play( note{ 0, 250000 } );
-	    player.play( note{ 698, 1000000 } );
-        
-        
-        
-        
+//         playNote(note{note::E5, note::dQ});
+//         playNote(note{note::D5s, note::dQ});
+//         playNote(note{note::E5, note::dQ});
+//         playNote(note{note::D5s, note::dQ});
+//         playNote(note{note::E5, note::dQ});
+//         playNote(note{note::B4, note::dQ});
+//         playNote(note{note::D5, note::dQ});
+//         playNote(note{note::C5, note::dQ});
+//         playNote(note{note::A4, note::dH});
+
+
+        player.play( note{ 523, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 698, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 880, 125000 } );
+        player.play( note{ 0, 125000 } );
+        player.play( note{ 0, 62500 } );
+        player.play( note{ 880, 250000 } );
+        player.play( note{ 0, 125000 } );
+        player.play( note{ 784, 500000 } );
+        player.play( note{ 523, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 698, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 784, 125000 } );
+        player.play( note{ 0, 125000 } );
+        player.play( note{ 0, 62500 } );
+        player.play( note{ 784, 250000 } );
+        player.play( note{ 0, 125000 } );
+        player.play( note{ 698, 250000 } );
+        player.play( note{ 659, 125000 } );
+        player.play( note{ 587, 250000 } );
+        player.play( note{ 523, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 698, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 698, 500000 } );
+        player.play( note{ 784, 250000 } );
+        player.play( note{ 659, 250000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 523, 250000 } );
+        player.play( note{ 261, 250000 } );
+        player.play( note{ 523, 250000 } );
+        player.play( note{ 784, 250000 } );
+        player.play( note{ 0, 250000 } );
+        player.play( note{ 698, 1000000 } );
+        player.play( note{ 523, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 698, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 880, 125000 } );
+        player.play( note{ 0, 125000 } );
+        player.play( note{ 0, 62500 } );
+        player.play( note{ 880, 250000 } );
+        player.play( note{ 0, 125000 } );
+        player.play( note{ 784, 500000 } );
+        player.play( note{ 523, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 698, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 1046, 500000 } );
+        player.play( note{ 659, 250000 } );
+        player.play( note{ 698, 250000 } );
+        player.play( note{ 659, 125000 } );
+        player.play( note{ 587, 250000 } );
+        player.play( note{ 523, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 698, 125000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 698, 500000 } );
+        player.play( note{ 784, 250000 } );
+        player.play( note{ 659, 250000 } );
+        player.play( note{ 587, 125000 } );
+        player.play( note{ 523, 250000 } );
+        player.play( note{ 261, 250000 } );
+        player.play( note{ 523, 250000 } );
+        player.play( note{ 784, 250000 } );
+        player.play( note{ 0, 250000 } );
+        player.play( note{ 698, 1000000 } );
+
     }
 
     void shoot()
@@ -217,10 +243,10 @@ private:
         {
             switch (state)
             {
-            case IDLE:
-            	
-            	hitSound();
-            	
+                case IDLE:
+
+                    hitSound();
+
 //                wait(soundIDChannel);
 //
 //                soundID = soundIDChannel.read();
@@ -230,33 +256,33 @@ private:
 //                    state = PLAY_SOUND;
 //                }
 
-                break;
-
-            case PLAY_SOUND:
-                switch (soundID)
-                {
-                case HITSOUND:
-                    hitSound();
-                    state = IDLE;
                     break;
 
-                case SHOOT:
-                    shoot();
-                    state = IDLE;
-                    break;
+                case PLAY_SOUND:
+                    switch (soundID)
+                    {
+                        case HITSOUND:
+                            hitSound();
+                            state = IDLE;
+                            break;
 
-                case GAMESTART:
-                    hitSound();
-                    state = IDLE;
-                    break;
+                        case SHOOT:
+                            shoot();
+                            state = IDLE;
+                            break;
 
-                case GAMEOVER:
-                    shoot();
-                    state = IDLE;
-                    break;
-                };
+                        case GAMESTART:
+                            hitSound();
+                            state = IDLE;
+                            break;
 
-                break;
+                        case GAMEOVER:
+                            shoot();
+                            state = IDLE;
+                            break;
+                    };
+
+                    break;
             };
         }
     }
